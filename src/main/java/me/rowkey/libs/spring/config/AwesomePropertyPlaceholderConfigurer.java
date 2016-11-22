202993d0a04c4a7736097f557ae5e4152058a691
superhj1987 <superhj1987@126.com>
2016-11-22 18:18:35 +0800

    Enhancement: refactor awesome propertyplaceholderconfigure

--- src/main/java/me/rowkey/libs/spring/config/AwesomePropertyPlaceholderConfigurer.java
+++ src/main/java/me/rowkey/libs/spring/config/AwesomePropertyPlaceholderConfigurer.java
@@ -1,13 +1,13 @@
 package me.rowkey.libs.spring.config;
 
-import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
 /**
  * Author: Bryant
  * Date: 14/11/20
  * Time: 上午10:09
  */
-public class AwesomePropertyPlaceholderConfigurer extends PropertyPlaceholderConfigurer {
+public class AwesomePropertyPlaceholderConfigurer extends PropertySourcesPlaceholderConfigurer {
     public AwesomePropertyPlaceholderConfigurer() {
         super();
         setPropertiesPersister(new AwesomePropertiesPersister());
